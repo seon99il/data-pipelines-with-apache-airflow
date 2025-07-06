4602e660c473b7be98e9fbb2a02f93e82ede19ee
Seonggil Jeong <sgjeong1108@icloud.com>
2025-07-07 03:21:31 +0900

    chapter3: 원자성, 멱등성

--- dags/chapter3/01_unscheduled.py
+++ dags/chapter3/01_unscheduled.py
@@ -10,18 +10,24 @@ with DAG(
     dag_id='01_unscheduled',
     start_date=dt.datetime(2025, 1, 1),
     schedule_interval=dt.timedelta(days=3),  # 3일 간격으로 실행
+    catchup=False,
 ) as dag:
   fetch_events = BashOperator(
       task_id='fetch_events',
       bash_command=(
         "mkdir -p /tmp/events && "
-        "curl -o /data/events.json -L https://localhost:5000/events"
+        "curl -o /data/events/{{ds}}.json -L https://localhost:5000/events?"
+        "start_date={{ds}}"
+        "&{{next_ds}}"
       ),
       dag=dag
   )
 
 
-  def _calculate_stats(input_path: str, output_path: str):
+  def _calculate_stats(**context):
+    input_path = context['"templates_dict'][
+                   'input_path'] | '/data/events/{{ds}}.json'
+    output_path = context['templates_dict']['output_path']
     events = pd.read_json(input_path)
     stats = events.groupby(["date", 'user']).size().reset_index()
     Path(output_path).parent.mkdir(exist_ok=True)
@@ -32,8 +38,8 @@ with DAG(
       task_id="calculate_stats",
       python_callable=_calculate_stats,
       op_kwargs={
-        'input_path': '/data/events.json',
-        'output_path': '/data/stats.csv'
+        'input_path': '/data/events/{{ds}}.json',
+        'output_path': '/data/stats/{{ds}}.csv'
       },
       dag=dag
   )
