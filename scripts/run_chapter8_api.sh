816774a09b47876b349ddf65edbabb65343537c8
Seonggil Jeong <sgjeong1108@icloud.com>
2025-11-13 13:32:04 +0900

    chapter10: KubernetesPodOperator를 활용하여 컨테이너에서 태스크 실행하기

--- scripts/run_chapter8_api.sh
+++ scripts/run_chapter8_api.sh
@@ -1,3 +1,3 @@
 # use this line to fetch fresh data
-#python ../src/movielens-api/fetch_ratings.py --output_path ../src/movielens-api/ratings.csv
+#python ../src/movielens-api.yml/fetch_ratings.py --output_path ../src/movielens-api.yml/ratings.csv
 python ../src/movielens-api/app.py
